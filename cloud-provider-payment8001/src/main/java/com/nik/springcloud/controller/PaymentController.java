9f595ca1a0b3ed04ad14bf4d50dc6016ecbeb37d
hulk <xiaotong.t@aistarfish.com>
2021-07-01 23:33:52 +0800

    修改

--- cloud-provider-payment8001/src/main/java/com/nik/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/nik/springcloud/controller/PaymentController.java
@@ -1,12 +1,17 @@
 package com.nik.springcloud.controller;
 
+import javax.annotation.Resource;
+
 import com.nik.springcloud.entities.CommonResult;
 import com.nik.springcloud.entities.Payment;
 import com.nik.springcloud.service.PaymentService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
@@ -18,6 +23,7 @@ public class PaymentController {
     @PostMapping(value = "payment/create")
     public CommonResult create(@RequestBody Payment payment) {
         int res = paymentService.create(payment);
+        System.out.println("test");
         if (res > 0) {
             return new CommonResult(200,"插入成功",res);
         }else{
